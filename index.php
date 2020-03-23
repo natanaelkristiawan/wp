412981cf2594f83b96ddffcd181756ad6a239dfd
natanael kristiawan <natanaelkristiawan@maxsol.id>
2020-03-23 11:30:34 +0800

    update

--- index.php
+++ index.php
@@ -6,14 +6,8 @@
 
 			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
 
-			<?php get_template_part('loop'); ?>
-
-			<?php get_template_part('pagination'); ?>
-
 		</section>
 		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+	</main>	
 
 <?php get_footer(); ?>
